19ff51893fc2c530a8a9a780cf10fbdd074d01f0
Backend Developer <hseyn.mmmdzad@bk.ru>
2025-09-07 14:29:25 +0400

    Add files via upload

---
+++ DoWhileExample1.java
@@ -0,0 +1,17 @@
+package yenipabka;
+
+public class DoWhileExample1 {
+
+    public static void main(String[] args) {
+
+        int say = 1;
+        do {
+            System.out.println(say);
+            say++;
+
+
+        } while (say <= 5);
+    }
+
+
+}
